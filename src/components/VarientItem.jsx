635f695dd2b3027f946e8d8617168e0b783dccf1
Jayant <jayant@errsole.com>
2024-04-23 00:09:56 +0530

    added component DragHandle

--- src/components/VarientItem.jsx
+++ src/components/VarientItem.jsx
@@ -1,22 +1,6 @@
 import { useState } from "react";
-import { SortableElement, SortableHandle } from "react-sortable-hoc";
-
-const DragHandle = SortableHandle(() => (
-	<svg
-		width="14"
-		height="14"
-		viewBox="0 0 7 14"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<circle cx="1" cy="7" r="1" fill="black" fillOpacity="0.5" />
-		<circle cx="6" cy="7" r="1" fill="black" fillOpacity="0.5" />
-		<circle cx="1" cy="1" r="1" fill="black" fillOpacity="0.5" />
-		<circle cx="6" cy="1" r="1" fill="black" fillOpacity="0.5" />
-		<circle cx="1" cy="13" r="1" fill="black" fillOpacity="0.5" />
-		<circle cx="6" cy="13" r="1" fill="black" fillOpacity="0.5" />
-	</svg>
-));
+import { SortableElement } from "react-sortable-hoc";
+import DragHandle from "./DragHandle";
 
 const VarientItem = SortableElement(
 	({
